d40f8e0a2f2bba56e535f3968cdf9c74112793d9
unknown <Haleem-2707>
2024-08-29 13:02:36 -0700

    initial-commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import  './Header.css'
+import { BiMenuAltRight } from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler'
+
+
+export default function Header() {
+
+  const [menuOpened, setMenuOpened] = useState(false)
+
+  const getMenuStyles =(menuOpened) => {
+    if (document.documentElement.clientWidth <= 800)
+    {
+      return {
+         right: !menuOpened && "-100%"
+      }
+    }
+  }
+  return (
+    <section className="h-wrapper">
+        <div className="flexCenter paddings innerWidth h-container">
+
+            <img src="./logo.png" alt="logo" width={100}/>
+
+
+         <OutsideClickHandler 
+         onOutsideClick={()=> {
+          setMenuOpened(false)
+         }}
+         >
+            <div className="flexCenter h-menu"
+              style={getMenuStyles(menuOpened)}>
+                <a href="residence">Residencies</a>
+                <a href="values">Our Values</a>
+                <a href="contact">Contact Us</a>
+                <a href="getStarted">Get Started</a>
+                 <button className='button'>
+                 <a href="Contact">Contact</a>
+                 </button>
+              
+            </div>
+          </OutsideClickHandler>
+            <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
+          <BiMenuAltRight size={30}/>
+        </div>
+        </div>
+       
+    </section>
+
+    
+  )
+}
